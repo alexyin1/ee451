ebae37c9c5193ddeca2e4719c63e171a80ad2378
Alex Yin <alexyin@usc.edu>
2019-09-02 22:54:46 -0700

    part1 pa1

--- pa1/part1.cpp
+++ pa1/part1.cpp
@@ -1,41 +1,96 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include <time.h>
+#include <stdio.h>
+//#include <stdlib.h>
+#define BILLION 1E9
 
-int main(void){          // this example is about multiplying a 1024*1024 matrix with a vector
-  int i, j;
-  int n = 1024; 		 // matrix is 1024*1024
-  struct timespec start, stop; 
-  double time;
+void display_results(int n, struct timespec start, struct timespec stop, double** C){
+	double time = (stop.tv_sec - start.tv_sec)+ (double)(stop.tv_nsec - start.tv_nsec)/1e9;
 
-  double **matrix = (double**) malloc (sizeof(double*)*n);
-  for (i=0; i<n; i++) matrix[i] = (double*) malloc(sizeof(double)*n);
+	printf("Number of FLOPs = %lu, Execution time = %f sec,\n%lf MFLOPs per sec\n", (long)2*n*n*n, time, 1/time/1e6*2*n*n*n);		
+	printf("C[100][100]=%f\n", C[100][100]);
 
-  double * vector = (double *) malloc (sizeof(double)*n);
-  double * result = (double *) malloc (sizeof(double)*n);
-  for (i=0; i<n; i++){
-  	for (j=0; j<n; j++) matrix[i][j]=2;
-  	vector[i]=3;
-  	result[i]=0;
-  }
-  if( clock_gettime( CLOCK_REALTIME, &start) == -1 ) { perror( "clock gettime" );}
+	double accum = ( stop.tv_sec - start.tv_sec )
+	  + ( stop.tv_nsec - start.tv_nsec )
+	  / BILLION;
+	printf( "%lf seconds \n", accum );
+	return;
+}
 
-  // matrix * vector
-  for(i=0; i< n; i++)
-    for (j=0; j< n; j++)
-      result[i] = result[i] + matrix[i][j] * vector[j]; 
+void naive_matmul(int n, double**A, double**B, double** C){
+	for(int i=0; i<n; i++){
+		for(int j=0; j<n; j++){
+			C[i][j] = A[i][j] * B[j][i];   
+		}
+	}
+	return;
+}
 
-  
-  if( clock_gettime( CLOCK_REALTIME, &stop) == -1 ) { perror( "clock gettime" );}		
-  time = (stop.tv_sec - start.tv_sec)+ (double)(stop.tv_nsec - start.tv_nsec)/1e9;
+void block_matmul(int n, double**A, double**B, double** C, int block_size){
+	for(int i=0; i<n; i+=block_size){
+		for(int j=0; j<n; j+=block_size){
+			for(int row=0 ;row<block_size; row++){
+				for(int col=0; col<block_size; col++){
+					C[i+row][j+col] += A[i+row][j+col] * B[j+col][i+row];
+				}
+			}
+		}
+	}
+	return;
+}
 
-  printf("Number of FLOPs = %d, Execution time = %f sec,\n%f MFLOPs per sec\n", 2*n*n, time, 2*n*n/time/1e6);
-  
-  // release memory
-  free(vector);
-  free(result);
-  for (i=0; i<n; i++)
-  	free(matrix[i]);
-  	
-  return 0;
+
+void reset(int n, double**A, double**B, double** C){
+	for (int i=0; i<n; i++){
+		for(int j=0; j< n; j++){
+			A[i][j]=i;
+			B[i][j]=i+j;
+			C[i][j]=0;			
+		}
+	}
+	return;
+}
+
+
+int main(){
+	struct timespec start, stop;
+	int n = 4096;
+	//allocate memory
+	double ** A = new double*[n];
+	double ** B = new double*[n];
+	double ** C = new double*[n];
+	for (int i=0; i<n; i++){
+		A[i] = new double[n];
+		B[i] = new double[n];
+		C[i] = new double[n];
+	}
+
+	reset(n, A, B, C);
+//start timer
+	if( clock_gettime(CLOCK_REALTIME, &start) == -1) { perror("clock gettime");}
+	naive_matmul(n, A, B, C);
+//end timer 
+	if( clock_gettime( CLOCK_REALTIME, &stop) == -1 ) { perror("clock gettime");}	
+	display_results(n, start, stop, C);
+
+
+	int block_size = 16;
+	reset(n, A, B, C);
+//start timer
+	if( clock_gettime(CLOCK_REALTIME, &start) == -1) { perror("clock gettime");}
+	block_matmul(n, A, B, C, block_size);
+//end timer 
+	if( clock_gettime( CLOCK_REALTIME, &stop) == -1 ) { perror("clock gettime");}	
+	display_results(n, start, stop, C);
+
+
+
+	//deallocate memory
+	for (int i=0; i<n; i++){
+		delete A[i];
+		delete B[i];
+		delete C[i];
+	}
+	delete [] A;
+	delete [] B;
+	delete [] C;
 }
